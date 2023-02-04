6f1230d4171e365fe19eca415b0ac0a6a35cd517
Harsh <harshmange03@gmail.com>
2023-02-04 15:35:49 +0530

    Nav pages added as req

--- app/src/main/java/com/example/spit_hackathon_ecoquest/UserPage.java
+++ app/src/main/java/com/example/spit_hackathon_ecoquest/UserPage.java
@@ -41,7 +41,7 @@ public class UserPage extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.profile, R.id.weekly_report, R.id.task, R.id.settings)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_user_page);
